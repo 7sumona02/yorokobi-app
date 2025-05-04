418a0fbed7f7ad5bfbf48b4460d7906e9f45760b
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-05-04 06:58:14 +0530

    added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip'
+'use client'
+import React, { useState } from 'react'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 const Footer = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className='flex'>
-        <TooltipProvider>
-            <Tooltip>
-                <TooltipTrigger>
-                    <div className='group'>
-                        <div className='absolute left-2 bottom-1 opacity-100 group-hover:opacity-0 transition-all'>
-                            <img src='/assets/cat2.svg' alt='cat' className='w-full'/>
-                        </div>
-                        <div className='absolute left-2 bottom-1 opacity-0 group-hover:opacity-100 transition-all'>
+            <Popover>
+                <PopoverTrigger asChild>
+                    <div 
+                        className='group'
+                        onClick={() => setIsActive(!isActive)}
+                    >
+                        <div className={`absolute left-2 bottom-1 transition-all ${isActive ? 'opacity-0' : 'opacity-100'} group-hover:opacity-0`}>
                             <img src='/assets/cat1.svg' alt='cat' className='w-full'/>
                         </div>
-                    </div>
-                </TooltipTrigger>
-                <TooltipContent className='absolute -bottom-40 left-14'>
-                    <p>Add to library</p>
-                </TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
+                        <div className={`absolute left-2 bottom-1 transition-all ${isActive ? 'opacity-100' : 'opacity-0'} group-hover:opacity-100`}>
+                            <img src='/assets/cat2.svg' alt='cat' className='w-full'/>
+                        </div>
+                    </div>       
+                </PopoverTrigger>  
+                <PopoverContent className="bg-[#BECFBC] text-white font-[Franxurter] w-full text-sm -translate-y-[2vh] translate-x-14">
+                    <p>hello meowww</p>
+                </PopoverContent>
+            </Popover>       
         <div className='border-2 border-black rounded-full p-1 absolute right-4 bottom-4'>
             <img src='/assets/paw.svg' alt='paw' className='w-8'/>
         </div>
